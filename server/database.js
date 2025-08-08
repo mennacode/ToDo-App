e16b5f1204dc6f09c0278ac8c893667ece1edf60
Menna <mennaatesfaye1@gmail.com>
2025-08-08 07:21:23 -0400

    Update database

--- server/database.js
+++ server/database.js
@@ -30,3 +30,31 @@ const connectToMongoDB= async () => {
 
 const getConnectedClient = () => client;
 module.exports = { connectToMongoDB, getConnectedClient };
+
+
+
+
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA2LOpnLaqDQWjTiTen4-8yCQU1CU6-eJU",
+  authDomain: "todoapp-8a14d.firebaseapp.com",
+  projectId: "todoapp-8a14d",
+  storageBucket: "todoapp-8a14d.firebasestorage.app",
+  messagingSenderId: "772815850311",
+  appId: "1:772815850311:web:da100db0cc6c0c6fb1da62",
+  measurementId: "G-0EPNG889XQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
